675bd95216284ad0b4daf9ce8782897bec23b9a9
jeisonVargasGracialab <jeison.vargas@gracialab.com.co>
2021-05-08 16:22:15 -0500

    inicio del proyecto

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:prueba_flutter_ideati/page/home/home.dart';
+import 'package:prueba_flutter_ideati/routes.dart';
+
+void main() {
+  runApp(
+    EasyLocalization(
+      child: MyApp(),
+      saveLocale: true,
+      path: 'assets/translations',
+      fallbackLocale: Locale('en', 'US'),
+      supportedLocales: [
+        Locale('en', 'US'),
+        Locale('es', 'ES'),
+        Locale('es', 'CO')
+      ],
+    ),
+  );
+}
+
+class MyApp extends StatefulWidget {
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.grey,
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      routes: {
+        '/': (BuildContext context) {
+          return Home();
+        },
+        ...Routes.getRoutes(context: context)
+      },
+    );
+  }
+}
---
+++ lib/routes.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:prueba_flutter_ideati/page/account/account.dart';
+import 'package:prueba_flutter_ideati/page/home/home.dart';
+
+class Routes {
+  static getRoutes({BuildContext context}) {
+    return {
+      '/': (BuildContext context) => Home(),
+      '/account': (BuildContext context) => Account(),
+    };
+  }
+}
+
+
